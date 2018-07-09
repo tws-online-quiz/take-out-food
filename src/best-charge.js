4b3a8d8eeda2aadd575f24320b78de180b187975
louis <louis.z.j.li@oocl.com>
2018-07-09 13:11:31 +0800

    [louis] optimize the code with lamda expression

--- src/best-charge.js
+++ src/best-charge.js
@@ -3,26 +3,12 @@ const {loadPromotions} = require('./promotions')
 //单个食物信息匹配
 matchFoodInfo=(foodId)=>{
   let allfoodItems =  loadAllItems()
-  let foodItemInfo = ''
-  allfoodItems.map(item=>{
-    if(item.id === foodId){
-      foodItemInfo = item
-      return
-    } 
-  })
-  return foodItemInfo
+  return allfoodItems.find(item=>item.id === foodId)
 }
 
 //单个食物数量计算
 calculateFoodNumber=(foodId,orderList)=>{
-  let num = 0
-  orderList.map(item=>{
-    if(item.split('x')[0].trim() === foodId){
-      num = parseInt(item.split('x')[1].trim())
-      return
-    } 
-  })
-  return num
+  return parseInt(orderList.find(item=>item.split('x')[0].trim() === foodId).split('x')[1].trim())
 }
 
 //计算单个食物小计
@@ -43,42 +29,23 @@ buildFoodList=(orderList)=>{
 
 //单个商品是否属于半价优惠类型
 isHalfpricePromotion=(foodId)=>{
-  let isHalf = false
   let allPromotionList = loadPromotions()
-  allPromotionList.map(promotionItem=>{
-    if(promotionItem.type === '指定菜品半价' && promotionItem.items && promotionItem.items.includes(foodId)){
-      isHalf = true
-      return
-    }
-  })
+  let key = allPromotionList.find(promotionItem=>promotionItem.type === '指定菜品半价' && promotionItem.items && promotionItem.items.includes(foodId))
+  let isHalf = key ? true :false
   return isHalf
 }
 
-//单个商品是否属于半价优惠类型
+//半价优惠商品总价
 countHalfpricePromotionTotalMoney = (halfFoodList)=>{
-  let HalfpricePromotionTotalMoney = 0
-  halfFoodList.map(item=>{
-    HalfpricePromotionTotalMoney += item.subPrice/2
-  })
-  return HalfpricePromotionTotalMoney
+  return halfFoodList.reduce((result,item)=>result + item.subPrice/2,0)
 }
 //生成半价商品数组
 buildHalfPriceList=(foodList)=>{
-  let halfFoodList = []
-  foodList.map(foodItem=>{
-    if(isHalfpricePromotion(foodItem.foodItemInfo.id)){
-      halfFoodList.push(foodItem)
-    }
-  })
-  return halfFoodList
+  return foodList.filter(foodItem=>isHalfpricePromotion(foodItem.foodItemInfo.id))
 }
 //计算食物列表原来的总价
 countOriginalPrice=(foodList)=>{
-  let originalPrice = 0
-  foodList.map(item=>{
-    originalPrice +=item.subPrice
-  })
-  return originalPrice
+  return foodList.reduce((result,item)=>result + item.subPrice,0)
 }
 
 //生成优惠信息
@@ -124,10 +91,7 @@ ${promotionString}
 
 //生成订单字符串
 buildOrderDetailString=(foodList,promotionString,TotalMoney)=>{
-  let foodListString = ''
-  foodList.map(item=>{
-    foodListString += `\n${item.foodItemInfo.name} x ${item.foodNum} = ${item.subPrice}元`
-  }) 
+  let foodListString = foodList.reduce((result,item)=>result + `\n${item.foodItemInfo.name} x ${item.foodNum} = ${item.subPrice}元`,'') 
   let orderInfo = `
 ============= 订餐明细 =============${foodListString}
 -----------------------------------${promotionString}
