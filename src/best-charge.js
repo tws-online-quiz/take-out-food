e28adfe5cf94b15cbe0e62819faa960a9fa26e09
LOUIS Z J LI (ITA-ISDC-ISD-OOCLL/ZHA) <LILO4@OOCL.COM>
2018-07-09 09:04:05 +0800

    [louis] optimize the code

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,31 +1,5 @@
 const {loadAllItems} = require('./items')
 const {loadPromotions} = require('./promotions')
-let promotionInfo = {
-  "type": "指定菜品半价",
-  "halfPriceList": [
-    {
-      "foodItemInfo": {
-        "id": "ITEM0013",
-        "name": "肉夹馍",
-        "price": 6.00
-      },
-      "foodNum": 2,
-      "subPrice": 12
-    },
-    {
-      "foodItemInfo": {
-        "id": "ITEM0022",
-        "name": "凉皮",
-        "price": 8.00
-      },
-      "foodNum": 1,
-      "subPrice": 8
-    }
-  ],
-  "saveMoney": 13
-}
-let orderPrice = 25
-
 //单个食物信息匹配
 matchFoodInfo=(foodId)=>{
   let allfoodItems =  loadAllItems()
@@ -147,22 +121,29 @@ ${promotionString}
   }
   return totalString
 }
-//输出订单信息
-bestCharge=(orderList)=> {
+
+//生成订单字符串
+buildOrderDetailString=(foodList,promotionString,TotalMoney)=>{
   let foodListString = ''
-  let foodList = buildFoodList(orderList)
-  let PromotionInfo = buildPromotionInfo(orderList)
-  let promotionString = outputPromotionString(PromotionInfo)
-  let TotalMoney = countOriginalPrice(foodList) - PromotionInfo.saveMoney
-  for (let item of foodList) {
+  foodList.map(item=>{
     foodListString += `\n${item.foodItemInfo.name} x ${item.foodNum} = ${item.subPrice}元`
-  }
+  }) 
   let orderInfo = `
 ============= 订餐明细 =============${foodListString}
 -----------------------------------${promotionString}
 总计：${TotalMoney}元
 ===================================`
   return orderInfo;
+}
+
+//输出订单信息
+bestCharge=(orderList)=> {
+  
+  let foodList = buildFoodList(orderList)
+  let PromotionInfo = buildPromotionInfo(orderList)
+  let promotionString = outputPromotionString(PromotionInfo)
+  let TotalMoney = countOriginalPrice(foodList) - PromotionInfo.saveMoney
+  return buildOrderDetailString(foodList,promotionString,TotalMoney)
 }
 module.exports = {
   bestCharge,matchFoodInfo,calculateFoodNumber,countFoodSubPrice,buildFoodList,buildPromotionInfo,isHalfpricePromotion
